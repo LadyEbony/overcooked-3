2efd019df75667e9a13c3c2bfcd65ef38c7820e2
Siqi Tian <670908789tian@gmail.com>
2020-04-30 17:08:26 -0700

    cut and combine food

---
+++ Assets/Siqi/tsqScript/Food.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FOODType
+{
+    FISH,
+    TOMATO,
+    DISH01
+}
+public enum FOODStatus
+{
+    ORIGINAL,
+    SLICED,
+    COOKED
+}
+public class Food: MonoBehaviour
+{
+    public FOODType foodType;
+    public FOODStatus foodStatus;
+
+    public void slicing()
+    {
+        GetComponent<Animator>().SetBool("isSlicing", true);
+        foodStatus = FOODStatus.SLICED;
+    }
+
+    public void sliceInit()
+    {
+        GetComponent<Animator>().SetBool("isSlicing", false);
+        foodStatus = FOODStatus.ORIGINAL;
+    }
+}
