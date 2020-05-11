17f215a50651aae324842eee53d188bf93740fb1
SiqiT <670908789tian@gmail.com>
2020-05-11 10:07:24 -0700

    addfoodcabinets

---
+++ Assets/Siqi/Scripts/Food.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FOODType
+{
+    FISH,
+    APPLE,
+    DISH01
+}
+public enum FOODStatus
+{
+    ORIGINAL,
+    SLICING,
+    SLICED,
+    COOKING,
+    COOKED
+}
+public class Food: MonoBehaviour
+{
+    public FOODType foodType;
+    public FOODStatus foodStatus;
+
+    public void slicing()
+    {
+        foodStatus = FOODStatus.SLICING;
+        GetComponent<Animator>().SetBool("isSlicing", true);
+        foodStatus = FOODStatus.SLICED;
+    }
+
+    public void sliceInit()
+    {
+        GetComponent<Animator>().SetBool("isSlicing", false);
+        foodStatus = FOODStatus.ORIGINAL;
+    }
+}
